27e6127c22d55b4803c09540d2501da3a05c989d
EmmaUnimelb <eosman@unimelb.edu.au>
2024-05-27 11:59:47 +1000

    changes for storetest and overlap

--- scripts.js
+++ scripts.js
@@ -7,3 +7,12 @@ function saveSourceData(){
 function showSourceData(){
     document.getElementById("showdata").innerHTML=localStorage.getItem("transfer");
 }
+
+var box_sizes=["tiny_box","little_box","half_box","full_box"]
+function changeSizeClass(elID,clsName){
+    let element = document.getElementById(elID);
+    for(b in box_sizes ){
+        element.classList.remove(box_sizes[b]);
+    }
+    element.classList.add(clsName)
+}
